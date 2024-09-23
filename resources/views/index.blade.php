05ec8b112aa55176244a84214b60760ca5a81ac3
Otomewo Oluwatobi. E <otomewooluwatobi@gmail.com>
2024-09-24 00:46:15 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -323,11 +323,6 @@
         </defs>
     </svg>
 
-    {{-- <div class="preloader-wrapper">
-        <div class="preloader">
-        </div>
-    </div> --}}
-
     <header>
         <div class="container-fluid">
             <div class="row py-3 border-bottom">
@@ -1088,7 +1083,7 @@
 
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer-menu">
-                        <img src="images/logo.svg" width="240" height="70" alt="logo">
+                        <img src="{{ asset('landing_page/images/logo.svg') }}" width="240" height="70" alt="logo">
                         <div class="social-links mt-3">
                             <ul class="d-flex list-unstyled gap-2">
                                 <li>
@@ -1138,21 +1133,6 @@
                             <li class="menu-item">
                                 <a href="#" class="nav-link">About us</a>
                             </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Conditions </a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Our Journals</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Careers</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Affiliate Programme</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Ultras Press</a>
-                            </li>
                         </ul>
                     </div>
                 </div>
@@ -1170,13 +1150,7 @@
                                 <a href="#" class="nav-link">Stores</a>
                             </li>
                             <li class="menu-item">
-                                <a href="#" class="nav-link">Track Order</a>
-                            </li>
-                            <li class="menu-item">
                                 <a href="#" class="nav-link">Shop</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Info</a>
                             </li>
                         </ul>
                     </div>
@@ -1196,12 +1170,6 @@
                             </li>
                             <li class="menu-item">
                                 <a href="#" class="nav-link">Returns & Refunds</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Cookie Guidelines</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="#" class="nav-link">Delivery Information</a>
                             </li>
                         </ul>
                     </div>
