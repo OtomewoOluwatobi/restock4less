cf1334b3601b5a95d91312468f0f5e378676523c
Otomewo Oluwatobi. E <otomewooluwatobi@gmail.com>
2024-10-10 16:03:14 +0100

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $allProduct = Product::get();
+        $allProduct = Product::paginate(10);
         return View('admin', compact('allProduct'));
     }
 
